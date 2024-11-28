5cab5a3c63f83d766486fb169bcdb8077deffb03
serkanboyraci <serkanboyraci@gmail.com>
2024-11-28 11:04:07 +0100

    Initial Commit

---
+++ StanfordCS193p-1-Memorize/StanfordCS193p_1_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCS193p_1_MemorizeApp.swift
+//  StanfordCS193p-1-Memorize
+//
+//  Created by Ali serkan Boyracı  on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCS193p_1_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
