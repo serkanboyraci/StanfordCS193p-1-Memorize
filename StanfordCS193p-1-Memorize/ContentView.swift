2ce1895587889d491be65edffa4da9425843da65
serkanboyraci <serkanboyraci@gmail.com>
2024-11-30 13:46:52 +0100

    foregroundcolor added

--- StanfordCS193p-1-Memorize/ContentView.swift
+++ StanfordCS193p-1-Memorize/ContentView.swift
@@ -7,18 +7,35 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, CS139p!")
-        }
+struct ContentView: View { //1 ContentView behaves like a View
+    var body: some View { //3 some = herhangi bir view = ne olursa olsun goster demek gibi.
+        ZStack(content: { // Vstack altli ustlu olmasini sagliyor
+            // Zstack ayni dikez duzlemde olmasini sagliyor
+            //2  burdan padding sonuna kadar property dye gecer
+            // 6. content onuce ve padding oncesine parantez ekleyerek, Vstack icerigini belirttik.
+            // 5. aslinda burda 3 tane struct var birisi image digeri text sonuncusu da Vstack, Vstack() acilip icerisi degistirilebilir.
+           // Image(systemName: "globe")// 4 creating image struct, //6 systemName: systemde kayitli olan bir imagei cagiriyor. imageNamed olsaydi assetlerden bakacaktik.
+            
+            
+            
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundColor(.white) // 9. arka plani beyaz yapmak icin gerekli ancak hem foreground hemde storl ayni anda olmuyor o yuzden bir tane daha rectangle olusutuyoruz.
+            RoundedRectangle(cornerRadius: 15)// 8.wir müssen cornerradius geben.
+                            .strokeBorder(lineWidth: 3) // 9.kenarik icin
+            
+            Text("🥶").font(.largeTitle)
+                
+        })
+        .foregroundColor(Color.orange) // 7 functions to change color
+        .imageScale(.large) //7/ functions to make large
+        .foregroundStyle(.tint)
+        //5 creating text struct
         .padding()
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
